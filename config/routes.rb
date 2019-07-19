a9b89dcf0d79632a8291ad64959cc33973997d03
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-19 10:28:57 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,39 @@
 Rails.application.routes.draw do
+
+  scope module: :crm do
+
+  end
+
+  scope :admin, module: 'crm/admin', as: 'admin' do
+    resources :maintain_source_templates
+    resources :maintain_tag_templates
+    resources :maintains do
+      collection do
+        get :public
+        post :batch
+        get 'detect' => :new_detect
+        post 'detect' => :create_detect
+      end
+      member do
+        get :courses
+        get 'transfer' => :edit_transfer
+        patch 'transfer' => :update_transfer
+        patch :detach
+        patch :assume
+        get 'order' => :edit_order
+        patch 'order' => :update_order
+      end
+      resources :maintain_logs
+    end
+    resources :maintain_sources do
+      post :sync, on: :collection
+    end
+    resources :maintain_tags do
+      post :sync, on: :collection
+    end
+  end
+
+  scope :my, module: 'crm/my', as: 'my' do
+  end
+
 end
