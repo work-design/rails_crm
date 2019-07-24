e77b7e26e5d03a679a89c8f05d613d3c62c067e9
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-24 16:38:13 +0800

    crm edit pipiline

--- app/assets/javascripts/controllers/crm/admin/maintains/edit_transfer.js
+++ app/assets/javascripts/controllers/crm/admin/maintains/edit_transfer.js
@@ -1,2 +1,9 @@
-$('#maintain_pipeline_id').dropdown();
+$('#maintain_pipeline_id').dropdown({
+  onChange: function (value, text, $selectedItem) {
+    var search_url = new URL('/admin/pipelines/options', location.origin);
+    search_url.searchParams.set('pipeline_id', value);
+
+    Rails.ajax({url: search_url, type: 'GET', dataType: 'script'});
+  }
+});
 $('#maintain_member_id').dropdown();
