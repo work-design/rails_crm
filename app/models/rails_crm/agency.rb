7bba235d1da963770327252395f21a8856a87728
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-03 10:21:22 +0800

    upgrade agency

--- app/models/rails_crm/agency.rb
+++ app/models/rails_crm/agency.rb
@@ -4,6 +4,13 @@ module RailsCrm::Agency
   included do
     has_one :maintain, inverse_of: :agency
     has_many :maintains
+
+    before_validation :sync_from_maintain, if: -> { self.maintain.present? }
+  end
+
+  def sync_from_maintain
+    self.agent = maintain.agent
+    self.client = maintain.client
   end
   
 end
