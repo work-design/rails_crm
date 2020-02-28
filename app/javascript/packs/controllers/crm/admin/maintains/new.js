43b7424ab94ae58ad53e328b41a44e90005e3a71
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-29 00:39:03 +0800

    use new api

--- app/javascript/packs/controllers/crm/admin/maintains/new.js
+++ app/javascript/packs/controllers/crm/admin/maintains/new.js
@@ -1,8 +1,8 @@
 import 'rails_taxon/outer'
 
-$('#maintain_client_attributes_birthday_type').dropdown();
-$('#maintain_client_attributes_gender').dropdown();
-$('#maintain_tutelage_attributes_relation').dropdown();
-$('#maintain_tutelar_attributes_gender').dropdown();
-$('#maintain_maintain_source_id').dropdown();
-$('#maintain_pipeline_id').dropdown();
+$('#maintain_client_attributes_birthday_type').dropdown()
+$('#maintain_client_attributes_gender').dropdown()
+$('#maintain_tutelage_attributes_relation').dropdown()
+$('#maintain_tutelar_attributes_gender').dropdown()
+$('#maintain_maintain_source_id').dropdown()
+$('#maintain_pipeline_id').dropdown()
