ea699d5a15397f7cfecbb08441b59c35dc0b8667
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-25 15:23:25 +0800

    routes with path

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,41 @@
 Rails.application.routes.draw do
 
-  scope module: :crm, defaults: { namespace: 'application', business: 'crm' } do
-
-  end
-
-  scope :admin, module: 'crm/admin', as: :admin, defaults: { namespace: 'admin', business: 'crm' } do
-    resources :maintain_source_templates
-    resources :maintain_tag_templates
-    resources :maintains do
-      resources :maintain_logs
-      collection do
-        get :public
-        post :batch
-        get 'detect' => :new_detect
-        post 'detect' => :create_detect
-        get 'assign' => :new_batch_assign
-        post 'assign' => :create_batch_assign
+  namespace :crm, defaults: { business: 'crm' } do
+    namespace :admin, defaults: { namespace: 'admin' } do
+      resources :maintain_source_templates
+      resources :maintain_tag_templates
+      resources :maintains do
+        resources :maintain_logs
+        collection do
+          get :public
+          post :batch
+          get 'detect' => :new_detect
+          post 'detect' => :create_detect
+          get 'assign' => :new_batch_assign
+          post 'assign' => :create_batch_assign
+        end
+        member do
+          get 'transfer' => :edit_transfer
+          patch 'transfer' => :update_transfer
+          get 'assign' => :edit_assign
+          patch 'assign' => :update_assign
+          patch :assume
+          patch :detach
+          get :orders
+          get 'order' => :edit_order
+          patch 'order' => :update_order
+        end
       end
-      member do
-        get 'transfer' => :edit_transfer
-        patch 'transfer' => :update_transfer
-        get 'assign' => :edit_assign
-        patch 'assign' => :update_assign
-        patch :assume
-        patch :detach
-        get :orders
-        get 'order' => :edit_order
-        patch 'order' => :update_order
+      resources :maintain_sources do
+        post :sync, on: :collection
+      end
+      resources :maintain_tags do
+        post :sync, on: :collection
       end
     end
-    resources :maintain_sources do
-      post :sync, on: :collection
-    end
-    resources :maintain_tags do
-      post :sync, on: :collection
-    end
-  end
 
-  scope :my, module: 'crm/my', as: :my, defaults: { namespace: 'my', business: 'crm' } do
+    namespace :my, defaults: { namespace: 'my' } do
+    end
   end
 
 end
