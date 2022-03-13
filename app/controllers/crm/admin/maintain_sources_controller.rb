fdfed7569f6e8a3f67861b63042e3570f522aaa9
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-13 15:34:19 +0800

    sync

--- app/controllers/crm/admin/maintain_sources_controller.rb
+++ app/controllers/crm/admin/maintain_sources_controller.rb
@@ -11,7 +11,7 @@ module Crm
     end
 
     def templates
-      @maintain_source_templates = MaintainSourceTemplate.order(id: :asc)
+      @maintain_source_templates = Source.order(id: :asc)
       @selected_maintain_sources = MaintainSource.where.not(maintain_source_template_id: nil).default_where(q_params).order(maintain_source_template_id: :asc)
     end
 
