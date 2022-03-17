09acf0922fd13da8a3cb6b8bcadd7d4c7cf8974f
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-18 01:06:35 +0800

    optioal true

--- app/models/crm/model/source_contact.rb
+++ app/models/crm/model/source_contact.rb
@@ -5,8 +5,8 @@ module Crm
     included do
       belongs_to :maintain_source
       belongs_to :source
-      belongs_to :contact, class_name: 'Wechat::Contact'
-      belongs_to :trade_item, class_name: 'Trade::TradeItem'
+      belongs_to :contact, class_name: 'Wechat::Contact', optional: true
+      belongs_to :trade_item, class_name: 'Trade::TradeItem', optional: true
     end
 
     def url_with_watermark
--- app/models/crm/source.rb
+++ app/models/crm/source.rb
@@ -6,7 +6,7 @@ module Crm
     def order_paid(trade_item)
       maintain_source = maintain_sources.find_or_initialize_by(organ_id: trade_item.member_organ_id)
 
-      mc = material_contacts.build
+      mc = source_contacts.build
       mc.trade_item = trade_item
       mc.maintain_source = maintain_source
 
