2c1e115ae24f00db9490d3e03bc6c325b17a915d
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-17 18:02:10 +0800

    use try method

--- app/models/crm/model/material/qrcode_material.rb
+++ app/models/crm/model/material/qrcode_material.rb
@@ -2,7 +2,7 @@ module Crm
   module Model::Material::QrcodeMaterial
 
     def watermark
-      "/watermark/url/#{Base64.urlsafe_encode64('/meal_dev/' + picture.key)}/align/#{align}/margin/#{margin_x}x#{margin_y}"
+      "/watermark/url/#{Base64.urlsafe_encode64('/' + [picture.service.try(:folder), picture.key].compact.join('/'))}/align/#{align}/margin/#{margin_x}x#{margin_y}"
     end
 
   end
