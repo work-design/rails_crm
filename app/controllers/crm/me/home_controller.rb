2b4afc89a694aba98432700f90c335a8b1496016
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-20 00:44:02 +0800

    crm

--- app/controllers/crm/me/home_controller.rb
+++ app/controllers/crm/me/home_controller.rb
@@ -2,7 +2,10 @@ module Crm
   class Me::HomeController < Me::BaseController
 
     def index
-
+      @wechat_user = current_user.oauth_users.find_by(appid: current_wechat_app&.appid)
+      unless @wechat_user
+        @url = current_wechat_app.oauth2_url(state: current_account.identity, action: 'bind')
+      end
     end
 
   end
