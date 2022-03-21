bc851acfbf26aef4e1a7eec33c716da524f4150e
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-21 10:06:35 +0800

    support list

--- app/controllers/crm/me/maintain_sources_controller.rb
+++ app/controllers/crm/me/maintain_sources_controller.rb
@@ -1,6 +1,7 @@
 module Crm
   class Me::MaintainSourcesController < Me::BaseController
     before_action :set_sources, only: [:index]
+    before_action :set_source, only: [:list, :source]
 
     def index
       q_params = {}
@@ -9,8 +10,11 @@ module Crm
       @maintain_sources = MaintainSource.default_where(q_params)
     end
 
+    def list
+      @sources = @source.children
+    end
+
     def source
-      @source = Source.find params[:source_id]
       @primary_material = @source.materials.find(&->(i){ i.is_a?(PrimaryMaterial) })
       if current_corp_user
         @contact = current_corp_user.contacts.find_or_initialize_by(state: @source.name)
@@ -20,7 +24,11 @@ module Crm
 
     private
     def set_sources
-      @sources = Source.all
+      @sources = Source.roots
+    end
+
+    def set_source
+      @source = Source.find params[:source_id]
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
       resources :maintain_sources do
         collection do
           get :source
+          get :list
         end
       end
     end
