89dd345b5e95d44e3adc5e55129f3a1f6c7bdc02
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-21 09:38:04 +0800

    sources

--- app/controllers/crm/panel/sources_controller.rb
+++ app/controllers/crm/panel/sources_controller.rb
@@ -1,12 +1,20 @@
 module Crm
   class Panel::SourcesController < Panel::BaseController
 
+    def index
+      q_params = {}
+      q_params.merge! params.permit(:name)
+
+      @sources = Source.roots.default_where(q_params).page(params[:page])
+    end
+
     private
     def source_params
       params.fetch(:source, {}).permit(
         :name,
         :price,
-        :logo
+        :logo,
+        :parent_ancestors
       )
     end
 
