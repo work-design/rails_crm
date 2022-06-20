f16c8f3be76bc8c45738ac739269e54e970d4dbf
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-20 14:37:36 +0800

    renmae file name

--- app/models/crm/ext/agent.rb
+++ app/models/crm/ext/agent.rb
@@ -1,9 +1,9 @@
-module Agential
+module Crm
   module Ext::Agent
     extend ActiveSupport::Concern
 
     included do
-      has_many :agencies, class_name: 'Agential::Agency', foreign_key: :agent_id, dependent: :delete_all
+      has_many :agencies, class_name: 'Crm::Agency', foreign_key: :agent_id, dependent: :delete_all
       has_many :clients, through: :agencies
 
       has_many :agent_maintains, class_name: 'Crm::Maintain', foreign_key: :agent_id, inverse_of: :agent
--- app/models/crm/ext/client.rb
+++ app/models/crm/ext/client.rb
@@ -1,9 +1,9 @@
-module Agential
+module Crm
   module Ext::Client
     extend ActiveSupport::Concern
 
     included do
-      has_many :agencies, class_name: 'Agential::Agency', dependent: :delete_all, inverse_of: :client
+      has_many :agencies, class_name: 'Crm::Agency', dependent: :delete_all, inverse_of: :client
       has_many :agents, through: :agencies
 
       has_many :client_maintains, class_name: 'Crm::Maintain', foreign_key: :client_id, inverse_of: :client
