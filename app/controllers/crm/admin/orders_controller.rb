00dd4efbb00e8168e7be22515ef3f6ce7362ad08
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-01 12:01:52 +0800

    orders

--- app/controllers/crm/admin/orders_controller.rb
+++ app/controllers/crm/admin/orders_controller.rb
@@ -2,6 +2,7 @@ module Crm
   class Admin::OrdersController < Trade::Admin::OrdersController
     before_action :set_maintain
     before_action :set_addresses, :set_new_order, only: [:new, :create]
+    before_action :set_payment_strategies, only: [:new, :create, :edit, :update]
 
     def index
       @orders = @maintain.orders.order(id: :desc).page(params[:page])
@@ -36,6 +37,10 @@ module Crm
 
     def set_addresses
       @addresses = @maintain.addresses.page(params[:page])
+    end
+
+    def set_payment_strategies
+      @payment_strategies = Trade::PaymentStrategy.default_where(default_params)
     end
 
   end
