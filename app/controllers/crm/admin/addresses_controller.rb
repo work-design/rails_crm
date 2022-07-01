c9ff2ff6173f15e94d8b0eb2924d59684cad39e4
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-02 00:28:20 +0800

    zh

--- app/controllers/crm/admin/addresses_controller.rb
+++ app/controllers/crm/admin/addresses_controller.rb
@@ -3,6 +3,7 @@ module Crm
     before_action :set_maintain
     before_action :set_address, only: [:show, :edit, :update, :destroy, :actions]
     before_action :set_new_address, only: [:new, :create, :order_new, :order_create, :from_new, :from_create]
+    before_action :set_addresses, only: [:order, :order_from, :order_create, :from_create]
 
     def index
       q_params = {}
@@ -12,7 +13,16 @@ module Crm
     end
 
     def new
-      @address.area ||= Profiled::Area.new
+    end
+
+    def order
+    end
+
+    def order_from
+    end
+
+    def from_create
+      @address.save
     end
 
     private
@@ -24,9 +34,13 @@ module Crm
       @address = @maintain.addresses.find(params[:id])
     end
 
+    def set_addresses
+      @addresses = @maintain.addresses.includes(:area).order(id: :desc).page(params[:page])
+    end
+
     def set_new_address
       @address = @maintain.addresses.build(address_params)
-      @address.area = Profiled::Area.new
+      @address.area ||= Profiled::Area.new
     end
 
     def _prefixes
