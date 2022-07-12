2a8fc8b07b66ea21e23d1cc8103fc7047732e218
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-12 18:53:33 +0800

    card purchases

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -1,11 +1,10 @@
 module Crm
   class Admin::CardPurchasesController < Trade::Admin::CardPurchasesController
+    before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
       @card_purchases = @card.card_purchases.order(last_expire_on: :desc).page(params[:page])
     end
-
-    private
-
+    
   end
 end
