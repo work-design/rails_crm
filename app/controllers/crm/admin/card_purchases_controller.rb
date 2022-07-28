ee6b9a76e8deaf0487e644dffb184eb572fae759
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-28 20:44:55 +0800

    gemfie

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -1,9 +1,15 @@
 module Crm
   class Admin::CardPurchasesController < Trade::Admin::CardPurchasesController
+    before_action :set_maintain
     before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
       @card_purchases = @card.card_purchases.order(last_expire_on: :desc).page(params[:page])
+    end
+
+    private
+    def set_maintain
+      @maintain = Maintain.find params[:maintain_id]
     end
 
   end
