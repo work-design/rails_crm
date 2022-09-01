969453d3d069064702c186e2cd0d04de0c39be6c
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-01 14:43:59 +0800

    fix cards admin in pc

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -1,6 +1,7 @@
 module Crm
   class Admin::CardPurchasesController < Trade::Admin::CardPurchasesController
     before_action :set_maintain
+    before_action :set_card
     before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
@@ -10,6 +11,11 @@ module Crm
     private
     def set_maintain
       @maintain = Maintain.find params[:maintain_id]
+      @client = @maintain.client
+    end
+
+    def set_card
+      @card = @client.cards.find params[:card_id]
     end
 
   end
