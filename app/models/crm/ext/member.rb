3c796209e021c2cd97fc07e611b9e308a928d65c
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-04 18:35:17 +0800

    add clients count

--- app/models/crm/ext/member.rb
+++ app/models/crm/ext/member.rb
@@ -3,6 +3,8 @@ module Crm
     extend ActiveSupport::Concern
 
     included do
+      attribute :maintains_count, :integer, default: 0
+
       has_many :maintains, ->(o) { where(organ_id: o.organ_id) }, class_name: 'Crm::Maintain'
       has_many :profile_clients, through: :maintains
     end
