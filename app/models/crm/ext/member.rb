79945b25f81b096b859511cc88a26f8f198990af
qinmingyuan <mingyuan0715@foxmail.com>
2022-09-04 18:25:08 +0800

    build maiints with organ id

--- app/models/crm/ext/member.rb
+++ app/models/crm/ext/member.rb
@@ -3,7 +3,7 @@ module Crm
     extend ActiveSupport::Concern
 
     included do
-      has_many :maintains, class_name: 'Crm::Maintain'
+      has_many :maintains, ->(o) { where(organ_id: o.organ_id) }, class_name: 'Crm::Maintain'
       has_many :profile_clients, through: :maintains
     end
 
