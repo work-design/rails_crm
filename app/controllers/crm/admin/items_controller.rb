ce0d1e890e17531bcafb1fbc767849d2e64ecdb8
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-20 12:06:00 +0800

    crm admin

--- app/controllers/crm/admin/items_controller.rb
+++ app/controllers/crm/admin/items_controller.rb
@@ -12,7 +12,7 @@ module Crm
     def set_new_item
       options = {}
       options.merge! client_id: @maintain.client_id, user_id: @maintain.client_user_id
-      options.merge! params.permit(:good_type, :good_id, :aim, :number, :produce_on, :scene_id, :fetch_oneself, :current_cart_id)
+      options.merge! params.permit(:good_type, :good_id, :aim, :number, :produce_on, :scene_id, :current_cart_id)
 
       @item = Trade::Item.new(options)
     end
