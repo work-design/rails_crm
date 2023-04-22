4af3821bcb1aae38703937607c3e852e911a9ebf
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-22 12:27:24 +0800

    set user id

--- app/controllers/crm/admin/items_controller.rb
+++ app/controllers/crm/admin/items_controller.rb
@@ -1,17 +1,13 @@
 module Crm
   class Admin::ItemsController < Trade::Admin::ItemsController
-    before_action :set_maintain
+    include Controller::Admin
+    before_action :set_common_maintain
     before_action :set_new_item, only: [:create]
 
     private
-    def set_maintain
-      @maintain = Maintain.find params[:maintain_id]
-      @client = @maintain.client
-    end
-
     def set_new_item
       options = {}
-      options.merge! client_id: @maintain.client_id, user_id: @maintain.client_user_id
+      options.merge! common_maintain_params
       options.merge! params.permit(:good_type, :good_id, :aim, :number, :produce_on, :scene_id, :current_cart_id)
 
       @item = Trade::Item.new(options)
--- app/controllers/crm/controller/admin.rb
+++ app/controllers/crm/controller/admin.rb
@@ -8,5 +8,14 @@ module Crm
       @client = @maintain.client.account&.user || @maintain.client.users[0] || @maintain.client
     end
 
+    def common_maintain_params
+      q = { client_id: @maintain.client_id }
+      user = @maintain.client.account&.user || @maintain.client.users[0]
+      if user
+        q.merge! user_id: @maintain.user.id
+      end
+      q
+    end
+
   end
 end
