e4155f32721b58b3df72ebcdeefbd7247ffa1fbb
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-24 00:24:38 +0800

    card purchase

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -5,10 +5,6 @@ module Crm
     before_action :set_card
     before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
 
-    def index
-      @card_purchases = @card.card_purchases.order(last_expire_on: :desc).page(params[:page])
-    end
-
     private
     def set_card
       @card = @client.cards.find params[:card_id]
