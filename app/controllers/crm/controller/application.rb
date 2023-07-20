199a60c44bfebd53816ec4796e3cf2edf5eda8d6
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-20 15:36:31 +0800

    with common maintain id

--- app/controllers/crm/controller/application.rb
+++ app/controllers/crm/controller/application.rb
@@ -3,12 +3,11 @@ module Crm
     extend ActiveSupport::Concern
 
     def common_maintain_params
-      q = { client_id: @maintain.client_id }
-      user = @maintain.client.account&.user || @maintain.client.users[0]
-      if user
-        q.merge! user_id: @maintain.user.id
-      end
-      q
+      {
+        maintain_id: @maintain.id,
+        client_id: @maintain.client_id,
+        user_id: @maintain.client_user_id
+      }
     end
 
     def set_client_user
