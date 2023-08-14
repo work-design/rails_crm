94f7b59d96238f38c5b9dcb7fbaf4b3febbb2f71
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-14 22:20:16 +0800

    refxies

--- app/controllers/crm/me/cart/addresses_controller.rb
+++ app/controllers/crm/me/cart/addresses_controller.rb
@@ -1,9 +1,20 @@
 module Crm::Me
   class Cart::AddressesController < AddressesController
+    before_action :set_cart
 
     private
     def set_cart
       @cart = Trade::Cart.find params[:cart_id]
+    end
+
+    def _prefixes
+      super do |pres|
+        if ['index'].include?(params[:action])
+          pres = ["ship/my/addresses/_#{params[:action]}", 'ship/my/addresses/_base', 'ship/my/addresses']
+        else
+          pres
+        end
+      end
     end
 
   end
