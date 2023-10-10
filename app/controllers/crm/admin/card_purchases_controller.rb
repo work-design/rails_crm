60158ac2b7a9cc3374c22134e79ce44755f23ea0
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-10 17:26:21 +0800

    share method

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -2,8 +2,6 @@ module Crm
   class Admin::CardPurchasesController < Trade::Admin::CardPurchasesController
     include Controller::Admin
     before_action :set_common_maintain
-    before_action :set_card
-    before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
 
     private
     def set_card
