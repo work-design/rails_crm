a5c28773792a7d002ddce54a0d37c734a3e8792e
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-10 17:28:53 +0800

    add before filter

--- app/controllers/crm/admin/card_purchases_controller.rb
+++ app/controllers/crm/admin/card_purchases_controller.rb
@@ -2,6 +2,9 @@ module Crm
   class Admin::CardPurchasesController < Trade::Admin::CardPurchasesController
     include Controller::Admin
     before_action :set_common_maintain
+    before_action :set_card
+    before_action :set_card_purchase, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_card_purchase, only: [:new, :create]
 
     private
     def set_card
