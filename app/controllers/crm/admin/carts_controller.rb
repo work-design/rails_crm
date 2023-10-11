d98c04228fd4cd9e1813df33ee42b553bfe01d63
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-11 18:21:06 +0800

    cart from my

--- app/controllers/crm/admin/carts_controller.rb
+++ app/controllers/crm/admin/carts_controller.rb
@@ -1,7 +1,9 @@
 module Crm
-  class Admin::CartsController < Trade::Admin::CartsController
+  class Admin::CartsController < Trade::My::CartsController
     include Controller::Admin
     before_action :set_common_maintain
+    before_action :set_cart, only: [:show, :edit, :update]
+    before_action :set_purchase, only: [:show]
 
     def index
       @carts = @maintain.carts.page(params[:page])
@@ -12,6 +14,11 @@ module Crm
 
       @items = @cart.items.includes(produce_plan: :scene).default_where(q_params).order(id: :asc).page(params[:page])
       @checked_ids = @cart.items.default_where(q_params).unscope(where: :status).status_checked.pluck(:id)
+    end
+
+    private
+    def set_cart
+      @cart = @maintain.carts.find params[:id]
     end
 
   end
