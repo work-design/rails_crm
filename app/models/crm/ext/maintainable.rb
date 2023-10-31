bc681f7f9c8277266dcb1fba568354ffce3385d0
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-31 13:09:48 +0800

    i18n

--- app/models/crm/ext/maintainable.rb
+++ app/models/crm/ext/maintainable.rb
@@ -3,7 +3,7 @@ module Crm
     extend ActiveSupport::Concern
 
     included do
-      belongs_to :maintain, class_name: 'Crm::Maintain', counter_cache: true, optional: true
+      #belongs_to :maintain, class_name: 'Crm::Maintain', counter_cache: true, optional: true
       belongs_to :client, class_name: 'Profiled::Profile', optional: true
       belongs_to :agent, class_name: 'Org::Member', optional: true
       accepts_nested_attributes_for :client
