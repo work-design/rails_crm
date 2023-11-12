db9ec4d4fdbeb1f8d37c6c62b4bd1744d89468dd
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-12 20:32:31 +0800

    add count cache to client

--- app/models/crm/ext/client.rb
+++ app/models/crm/ext/client.rb
@@ -3,16 +3,23 @@ module Crm
     extend ActiveSupport::Concern
 
     included do
+      attribute :wallets_count, :integer, default: 0
+      attribute :cards_count, :integer, default: 0
+      attribute :orders_count, :integer, default: 0
+      attribute :addresses_count, :integer, default: 0
+      attribute :items_count, :integer, default: 0
+      attribute :carts_count, :integer, default: 0
+
       has_many :agencies, class_name: 'Crm::Agency', inverse_of: :client, dependent: :delete_all
       has_many :agents, through: :agencies
 
       has_one :lawful_wallet, class_name: 'Trade::LawfulWallet', foreign_key: :client_id
 
       has_many :client_maintains, class_name: 'Crm::Maintain', foreign_key: :client_id, inverse_of: :client
-      has_many :orders, class_name: 'Trade::Order', foreign_key: :client_id, dependent: :nullify
       has_many :addresses, class_name: 'Profiled::Address', foreign_key: :client_id, dependent: :nullify
-      has_many :wallets, class_name: 'Trade::Wallet', foreign_key: :client_id, dependent: :nullify
       has_many :cards, class_name: 'Trade::Card', foreign_key: :client_id, dependent: :nullify
+      has_many :orders, class_name: 'Trade::Order', foreign_key: :client_id, dependent: :nullify
+      has_many :wallets, class_name: 'Trade::Wallet', foreign_key: :client_id, dependent: :nullify
 
       after_save_commit :sync_user_later, if: -> { account && saved_change_to_identity? }
     end
