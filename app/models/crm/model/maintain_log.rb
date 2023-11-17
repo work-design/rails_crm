24256a43d9630a597cb355e919c7ed3000b2541f
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-17 15:10:53 +0800

    change client

--- app/models/crm/model/maintain_log.rb
+++ app/models/crm/model/maintain_log.rb
@@ -9,8 +9,9 @@ module Crm
       attribute :extra, :json
 
       belongs_to :member, class_name: 'Org::Member'
+      belongs_to :client, class_name: 'Profiled::Profile'
 
-      belongs_to :maintain
+      belongs_to :maintain, optional: true
       belongs_to :logged, polymorphic: true, optional: true
       belongs_to :maintain_tag, counter_cache: true, optional: true
     end
