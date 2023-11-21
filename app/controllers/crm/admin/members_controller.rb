edfbd1ae7beae9f3dd43b427aab323c819a1a660
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-21 09:40:09 +0800

    direct add member to organs

--- app/controllers/crm/admin/members_controller.rb
+++ app/controllers/crm/admin/members_controller.rb
@@ -2,6 +2,7 @@ module Crm
   class Admin::MembersController < Admin::BaseController
     before_action :set_client_organ
     #before_action :set_client, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_member, only: [:new, :create]
 
     def index
       @maintains = Maintain.includes(:client_member, :client_organ).where(client_organ_id: params[:client_organ_id]).page(params[:page])
@@ -12,11 +13,22 @@ module Crm
 
     private
     def modal_name
-      'client'
+      'member'
     end
 
     def set_client_organ
       @client = Org::Organ.where.associated(:client_maintains).where(client_maintains: { organ_id: current_organ.id }).find params[:client_organ_id]
+    end
+
+    def set_new_member
+      @member = @client.members.build(member_params)
+    end
+
+    def member_params
+      params.fetch(:member, {}).permit(
+        :identity,
+        :name
+      )
     end
 
   end
