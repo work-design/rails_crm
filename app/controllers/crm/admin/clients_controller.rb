d92a84326b769c6bc1a5980abf7459863772e13b
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-21 12:22:19 +0800

    the clients files

--- app/controllers/crm/admin/clients_controller.rb
+++ app/controllers/crm/admin/clients_controller.rb
@@ -1,6 +1,7 @@
 module Crm
   class Admin::ClientsController < Admin::BaseController
     before_action :set_client, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_client, only: [:new, :create]
 
     def index
       @maintains = Maintain.includes(:client, :client_member, :client_organ).where.not(client_member_id: nil).page(params[:page])
@@ -14,8 +15,20 @@ module Crm
       'client'
     end
 
+    def set_new_client
+      @client = Profiled::Profile.new(client_params)
+    end
+
     def set_client
       @client = Org::Member.where.associated(:client_maintains).where(client_maintains: { organ_id: current_organ.id }).find params[:id]
+    end
+
+    def client_params
+      _p = params.fetch(:client, {}).permit(
+        :identity,
+        :nick_name
+      )
+      _p.merge! default_form_params
     end
 
   end
