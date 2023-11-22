5d3c6d49ae595aaf77a44940b896abdd5f2f3026
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-22 20:23:00 +0800

    client id

--- app/controllers/crm/controller/admin.rb
+++ app/controllers/crm/controller/admin.rb
@@ -10,7 +10,9 @@ module Crm
     private
     def set_common_maintain
       if params[:client_id]
-        @client = Profiled::Profile.default_where(default_ancestors_params).find params[:client_id]
+        @client = Client.default_where(default_ancestors_params).find params[:client_id]
+      elsif params[:contact_id]
+        @client = Contact.default_where(default_ancestors_params).find params[:contact_id]
       elsif params[:client_member_id]
         @client = Org::Member.where.associated(:client_maintains).where(client_maintains: { organ_id: current_organ.id }).find params[:client_member_id]
       elsif params[:client_organ_id]
