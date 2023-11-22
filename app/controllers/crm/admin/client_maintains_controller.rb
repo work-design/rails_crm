f9df8b45f55068edd0cdcbad7c5191a33c743e6a
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-22 17:41:01 +0800

    fix edit and update

--- app/controllers/crm/admin/client_maintains_controller.rb
+++ app/controllers/crm/admin/client_maintains_controller.rb
@@ -3,11 +3,26 @@ module Crm
   class Admin::ClientMaintainsController < Admin::BaseController
     include Controller::Admin
     before_action :set_common_maintain
+    before_action :set_maintain, only: [:show, :edit, :update, :destroy, :actions]
 
     def index
       @maintains = @client.client_maintains.page(params[:page])
     end
 
+    private
+    def model_name
+      'maintain'
+    end
+
+    def set_maintain
+      @maintain = Maintain.find params[:id]
+    end
+
+    def maintain_params
+      params.fetch(:maintain, {}).permit(
+        :remark
+      )
+    end
 
   end
 end
