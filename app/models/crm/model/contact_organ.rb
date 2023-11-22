67bca26c79de1448a08868893c1f46d066c3113e
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-22 18:32:30 +0800

    add attr define

--- app/models/crm/model/contact_organ.rb
+++ app/models/crm/model/contact_organ.rb
@@ -2,6 +2,14 @@
 
 module Crm
   module Model::ContactOrgan
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :settings, :json, default: {}
+
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
+      belongs_to :client_organ, class_name: 'Org::Organ', optional: true
+    end
 
   end
 end
