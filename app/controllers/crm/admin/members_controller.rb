97f784d20566e52eaa7e36a2247ff47d07668d74
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-22 16:49:34 +0800

    members

--- app/controllers/crm/admin/members_controller.rb
+++ app/controllers/crm/admin/members_controller.rb
@@ -1,7 +1,7 @@
 module Crm
   class Admin::MembersController < Admin::BaseController
     before_action :set_client_organ
-    #before_action :set_client, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_member, only: [:show, :edit, :update, :destroy, :actions]
     before_action :set_new_member, only: [:new, :create]
 
     def index
@@ -18,6 +18,10 @@ module Crm
 
     def set_client_organ
       @client = Org::Organ.where.associated(:client_maintains).where(client_maintains: { organ_id: current_organ.id }).find params[:client_organ_id]
+    end
+
+    def set_member
+      @maintain = Maintain.find params[:id]
     end
 
     def set_new_member
