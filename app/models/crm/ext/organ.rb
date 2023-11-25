efbb89a64e944e3c9dd1e8c6c83d7204d9d0ef68
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-25 21:22:07 +0800

    fix ext ogran

--- app/models/crm/ext/organ.rb
+++ app/models/crm/ext/organ.rb
@@ -5,7 +5,7 @@ module Crm
     included do
       has_many :maintains, class_name: 'Crm::Maintain', dependent: :nullify
       has_many :client_maintains, class_name: 'Crm::Maintain', foreign_key: :client_organ_id
-      has_many :clients, foreign_key: :client_organ_id
+      has_many :clients, class_name: 'Crm::Client', foreign_key: :client_organ_id
     end
 
     def maintains_count
