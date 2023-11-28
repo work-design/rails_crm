a4a8c2d448554b8987dcbf39552ccb9f8cb7a103
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-28 13:05:51 +0800

    support per

--- app/controllers/crm/admin/clients_controller.rb
+++ app/controllers/crm/admin/clients_controller.rb
@@ -10,7 +10,7 @@ module Crm
       q_params = {}
       q_params.merge! default_params
 
-      @clients = Client.includes(:client_maintains).default_where(q_params).order(id: :desc).page(params[:page])
+      @clients = Client.includes(:client_maintains).default_where(q_params).order(id: :desc).page(params[:page]).per(params[:per])
     end
 
     def edit_assign
