ff2be5cbc1857ad70d0c7a3b4d5a54e6017f8d92
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-30 10:36:30 +0800

    for sub parent

--- app/controllers/crm/admin/client/clients_controller.rb
+++ app/controllers/crm/admin/client/clients_controller.rb
@@ -1,14 +1,31 @@
 module Crm
   class Admin::Client::ClientsController < Admin::ClientsController
-    before_action :set_client
+    before_action :set_parent
+    before_action :set_new_client, only: [:new, :create]
 
     def index
       @clients = @client.children.page(params[:page])
     end
 
     private
-    def set_client
+    def set_parent
       @client = Client.find params[:client_id]
     end
+
+    def set_new_client
+      @client = @client.children.build(client_params)
+    end
+
+    def client_params
+      _p = params.fetch(:client, {}).permit(
+        :name,
+        :parent_id,
+        :vendor,
+        :client_organ_id,
+        settings: {}
+      )
+      _p.merge! default_form_params
+    end
+
   end
 end
