8ac69c918e364c5b5ba50238f02a616d495b7b71
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-02 17:47:17 +0800

    crm me clients

---
+++ app/controllers/crm/me/clients_controller.rb
@@ -0,0 +1,6 @@
+module Crm
+  class Me::ClientsController < Admin::ClientsController
+    include Controller::Me
+
+  end
+end
---
+++ app/controllers/crm/me/contacts_controller.rb
@@ -0,0 +1,6 @@
+module Crm
+  class Me::ContactsController < Admin::ContactsController
+    include Controller::Me
+
+  end
+end
