fc32cf7f0553387d934a4ab0e3c62c5db9385365
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-03 16:12:00 +0800

    create with

--- app/controllers/crm/me/contacts_controller.rb
+++ app/controllers/crm/me/contacts_controller.rb
@@ -2,5 +2,14 @@ module Crm
   class Me::ContactsController < Admin::ContactsController
     include Controller::Me
 
+    def index
+      @contacts = current_member.agent_contacts.page(params[:page])
+    end
+
+    private
+    def set_new_contact
+      @contact = current_member.agent_contacts.build(contact_params)
+    end
+
   end
 end
