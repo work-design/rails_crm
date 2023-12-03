d2a9deff83b091d7f9cab68111716ca7cbc34ee3
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-03 16:22:19 +0800

    add clients

--- app/controllers/crm/me/clients_controller.rb
+++ app/controllers/crm/me/clients_controller.rb
@@ -2,5 +2,13 @@ module Crm
   class Me::ClientsController < Admin::ClientsController
     include Controller::Me
 
+    def index
+      @clients = current_member.agent_clients.page(params[:page])
+    end
+
+    private
+    def set_new_client
+      @client = current_member.agent_clients.build(client_params)
+    end
   end
 end
