c5c5aa68e2039f560746d429ddae810c6d2d18f8
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-03 16:10:03 +0800

    gem

--- app/models/crm/ext/member.rb
+++ app/models/crm/ext/member.rb
@@ -5,11 +5,12 @@ module Crm
     included do
       attribute :maintains_count, :integer, default: 0
 
-      has_many :maintains, ->(o) { where(organ_id: o.organ_id) }, class_name: 'Crm::Maintain'
-      has_many :client_maintains, class_name: 'Crm::Maintain', foreign_key: :client_member_id
-      has_many :contacts, foreign_key: :client_member_id
+      has_many :maintains, class_name: 'Crm::Maintain'
+      has_many :agent_contacts, class_name: 'Crm::Contact', through: :maintains, source: :contact
+      has_many :agent_clients, class_name: 'Crm::Client', through: :maintains, source: :client
 
-      has_many :addresses, class_name: 'Profiled::Address', foreign_key: :member_id
+      has_many :client_maintains, class_name: 'Crm::Maintain', foreign_key: :client_member_id
+      has_many :client_contacts, class_name: 'Crm::Contact', foreign_key: :client_member_id
     end
 
   end
