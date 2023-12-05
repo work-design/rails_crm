3e14d123f3c7b22e32f4495545b6cdb55dd9b7df
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-05 16:51:11 +0800

    i

--- app/imports/crm/maintain_import.rb
+++ app/imports/crm/maintain_import.rb
@@ -4,7 +4,7 @@ module Crm
 
     config do
       model Contact
-      column :name, header: 'My name', field: ->(i) { i}
+      column :name, header: 'My name', field: ->(i) { i.to_s }
       column :identity, header: 'Email', field: ->(i) { i }
     end
   end
