01aee67e7a562e39627ed81a544b1b66d3fe3a44
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-10 18:14:33 +0800

    prevent erros

--- app/controllers/crm/me/home_controller.rb
+++ app/controllers/crm/me/home_controller.rb
@@ -2,9 +2,11 @@ module Crm
   class Me::HomeController < Me::BaseController
 
     def index
-      @wechat_user = current_user.oauth_users.find_by(appid: current_wechat_app&.appid)
-      unless @wechat_user
-        @url = current_wechat_app.oauth2_url(state: current_account.identity, action: 'bind')
+      if defined? current_wechat_app
+        @wechat_user = current_user.oauth_users.find_by(appid: current_wechat_app&.appid)
+        unless @wechat_user
+          @url = current_wechat_app.oauth2_url(state: current_account.identity, action: 'bind')
+        end
       end
     end
 
