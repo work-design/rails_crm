9e0185e862abe288c1cac8686f6926c1e36c6ee8
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-30 22:18:25 +0800

    fixed errors

--- app/models/crm/model/maintain.rb
+++ app/models/crm/model/maintain.rb
@@ -17,7 +17,7 @@ module Crm
       belongs_to :task_template, class_name: 'Bench::TaskTemplate', optional: true if defined? RailsBench
       belongs_to :profile_agent, class_name: 'Profiled::Profile', foreign_key: :agent_id, optional: true
 
-      belongs_to :client, inverse_of: :client_maintains, optional: true
+      belongs_to :client, optional: true
       belongs_to :contact, optional: true
       belongs_to :agent, polymorphic: true, inverse_of: :agent_maintains, optional: true
       belongs_to :agency, optional: true
