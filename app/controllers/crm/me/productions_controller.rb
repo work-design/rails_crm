ce4a21c704de3e973426a597d849ad017dcabcb8
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-01 10:50:18 +0800

    updte

--- app/controllers/crm/me/productions_controller.rb
+++ app/controllers/crm/me/productions_controller.rb
@@ -1,6 +1,12 @@
 module Crm
   class Me::ProductionsController < Admin::ProductionsController
     include Controller::Me
+    before_action :set_payment, if: -> { params[:payment_id].present? }
+
+    private
+    def set_payment
+      @payment = Trade::Payment.find params[:payment_id]
+    end
 
   end
 end
