bf9fed0db2431f288ed47342a38edd38eae103bd
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-04 20:11:59 +0800

    crm

--- app/models/crm/model/contact.rb
+++ app/models/crm/model/contact.rb
@@ -39,7 +39,7 @@ module Crm
 
       validates :identity, uniqueness: { scope: [:client_id] }
 
-      before_save :sync_from_user, if: -> { client_user_id_changed? && client_user }
+      before_save :sync_from_client_user, if: -> { client_user_id_changed? && client_user }
       after_save :sync_member_to_orders, if: -> { (saved_changes.keys & ['client_member_id']).present? }
       after_save :sync_user_to_orders, if: -> { (saved_changes.keys & ['client_user_id']).present? }
       after_update :set_default, if: -> { default? && saved_change_to_default? }
