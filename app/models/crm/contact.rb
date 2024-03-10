0641dc6e6c21c8e1fbd7ef59c4508facbefffa16
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-10 17:18:24 +0800

    add audit for contact

--- app/models/crm/contact.rb
+++ app/models/crm/contact.rb
@@ -3,5 +3,9 @@ module Crm
     include Model::Contact
     include Ext::Agent
     include Wechat::Ext::Contact if defined? RailsWechat
+    if defined? RailsAudit
+      include Auditor::Ext::Discard
+      include Auditor::Ext::Audited
+    end
   end
 end
